bcd8eeee0ea09166048b0503df96bfd64a49db87
Miezhiko <Miezhiko@gmail.com>
2022-01-11 12:58:05 +0400

    2022
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- games-simulation/alchemy/alchemy-9999.ebuild
+++ games-simulation/alchemy/alchemy-9999.ebuild
@@ -1,4 +1,4 @@
-# Copyright 2021 Miezhiko
+# Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
 EAPI=7
---
+++ new_year.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -e
+
+digest() {
+	local e=$1
+	if [[ $e == *.ebuild ]]
+	then
+		ebuild "$e" digest
+	fi
+}
+
+copyright_year() {
+    local e=$1
+    echo 'FIX "# Copyright" in: '"$e"
+    sed -e 's/^# Copyright .*/# Copyright 1999-2022 Gentoo Authors/g' -i "${e}"
+    digest "$e"
+}
+
+legacy_header() {
+    local e=$1
+    echo 'FIX "# $Header:" in: '"$e"
+    sed -e 's/^# \$Header: .*\$$/# $Id$/g' -i "${e}"
+    digest "$e"
+}
+
+while read e; do
+    while read l
+    do
+        if [[ $l == '# Copyright 1999-'* ]]; then
+            copyright_year "$e"
+        fi
+        if [[ $l == '# $Header:'* ]]; then
+            legacy_header "$e"
+            break
+        fi
+    done < "$e"
+done < <(find . -type f)
