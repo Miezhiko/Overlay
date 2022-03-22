4d154c92796a7a7688fa2aed997f872474c414e5
Miezhiko <Miezhiko@gmail.com>
2022-03-22 17:19:11 +0400

    Drop stuff updated in portage!
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

---
+++ check_updates.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -e
+
+function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
+
+exceptions=(
+	"dev-util/boost-build"
+	"dev-libs/boost"
+)
+
+while read e; do
+	DIR="$(dirname "${e}")";
+	FILE="$(basename "${e}")";
+	vers=${FILE##*-};
+	if [[ $vers == r* ]]; then
+		vers=${FILE%-*}
+		vers=${vers##*-};
+	fi
+	vers=${vers%.ebuild*};
+	if [[ "$vers" != "9999" ]]; then
+		relative="${DIR:2}"
+		if [[ ! " ${exceptions[*]} " =~ " ${relative} " ]]; then
+			portage="/usr/portage/${relative}"
+			if [ -d "$portage" ]; then
+				while read p; do
+					FILEP="$(basename "${p}")";
+					versp=${FILEP##*-};
+					if [[ $versp == r* ]]; then
+						versp=${FILEP%-*}
+						versp=${versp##*-};
+					fi
+					versp=${versp%.ebuild*};
+					if [[ "$versp" != "9999" ]]; then
+						if [ $(version ${versp}) -gt $(version ${vers}) ]; then
+							echo "${relative}: ${versp} in portage, ${vers} in overlay!"
+						fi
+					fi
+				done < <(find "$portage" -type f -not -path '*/\.*' -name '*.ebuild')
+			fi
+		fi
+	fi
+done < <(find . -type f -not -path '*/\.*' -name '*.ebuild')
